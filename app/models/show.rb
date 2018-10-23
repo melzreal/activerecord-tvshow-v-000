2156d01e8b0bfc41d16faade3beefa539b9200b2
Melissa Real <melissa.real@hotmail.com>
2018-10-23 15:30:22 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -15,8 +15,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.least_popular_show
-    binding.pry
-    Show.find_by(self.lowest_rating)
+    Show.find_by('rating'=> self.lowest_rating )
   end
 
 
